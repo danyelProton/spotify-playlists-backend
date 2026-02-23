3930d92389bb4db2a96b0965b216d915e9bc3402
danyelProton <177734430+danyelProton@users.noreply.github.com>
2026-02-23 22:04:14 +0100

    Trying to fix Vercel deployment

--- api/index.js
+++ api/index.js
@@ -52,7 +52,7 @@ app.get('/favicon.ico', (req, res) => res.status(204).end());
 app.get('/api/albums', ApiController.getAlbumDataFromFile);
 app.get('/api/playlists', ApiController.getPlaylistDataFromFile);
 app.get('/api/updates', ApiController.getLastUpdateDataFromFile);
-app.all('/{*any}', (req, res, next) => {
+app.all('*', (req, res, next) => {
   throw new AppError(404, `Can't find ${req.originalUrl} on this server`);
 });
 app.use(errorHandler);
